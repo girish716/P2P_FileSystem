8d87f61bb824a68b56e416cb97cde1d7a8c1f2e3
Girish Chandra Dama <girish@Girishs-MacBook-Air.local>
2022-12-01 15:40:41 -0500

    resolved errors in Peer Client

--- src/MasterQuery.java
+++ src/MasterQuery.java
@@ -104,7 +104,7 @@ public class MasterQuery extends UnicastRemoteObject implements Master
      * @throws RemoteException
      */
     @Override
-    public String updateCathe(String path, String filename) throws RemoteException{
+    public String updateCache(String path, String filename) throws RemoteException{
         try {
             Set<String> paths;
             if(lookup.containsKey(filename)){
--- src/PeerClient.java
+++ src/PeerClient.java
@@ -2,68 +2,92 @@ import java.rmi.Naming;
 import java.util.*;
 
 public class PeerClient {
+    Integer masterPORT;
+    String masterIP;
+    Integer port;
+    String IP;
+
+    PeerClient(){
+        this.masterPORT = 1901;
+        this.masterIP = "localhost";
+        this.port = 1900;
+        this.IP = "localhost";
+    }
+
     public void createFile(Master masterServer){
-        //fetch IP and PORT of random peer
-        String peerData = masterServer.getPath();
-        // lookup method to find reference of remote object
-        FDS peerServer =
-                (FDS)Naming.lookup("rmi://"+peerData+"/master");
-        String response = peerServer.create("abc.txt", "Hello world");
-        System.out.println("File Successfully created....");
-        if(response!=null){
-            masterServer.updateLookup("rmi://"+IP+":"+port+"/master", response);
-            List<String> paths = masterServer.getPaths(response);
-            System.out.println(paths);
+        try{
+            //fetch IP and PORT of random peer
+            String peerData = masterServer.getPath();
+            // lookup method to find reference of remote object
+            FDS peerServer =
+                    (FDS)Naming.lookup("rmi://"+peerData+"/master");
+            String response = peerServer.create("abc.txt", "Hello world");
+            System.out.println("File Successfully created....");
+            if(response!=null){
+                masterServer.updateCache("rmi://"+this.IP+":"+this.port+"/master", response);
+                List<String> paths = masterServer.getPaths(response);
+                System.out.println(paths);
+            }
+            System.out.println(response);
         }
-        System.out.println(response);
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     public void readFile(Master masterServer, String fileName){
-        if(!masterServer.hasFile(fileName)) return;
+        try{
+            if(!masterServer.hasFile(fileName)) return;
 
-        List<String> paths = masterServer.getPaths(fileName);
-        String peerPath = paths.get(0);
+            List<String> paths = masterServer.getPaths(fileName);
+            String peerPath = paths.get(0);
 
-        // connect with server
-        FDS peerServer =
-                (FDS)Naming.lookup("rmi://"+peerData+"/master");
-        String fileData = peerServer.read(fileName)
-        if(fileData==null){
-            System.out.println("Failed to read file......")
+            // connect with server
+            FDS peerServer =
+                    (FDS)Naming.lookup("rmi://"+peerPath+"/master");
+            String fileData = peerServer.read(fileName);
+            if(fileData==null){
+                System.out.println("Failed to read file......");
+            }
+            System.out.println("File Data : "+ fileData);
         }
-        System.out.println("File Data : "+ fileData);
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     public void updateFile(Master masterServer, String fileName, String updatedData){
-        if(!masterServer.hasFile(fileName)) return;
+        try{
+            if(!masterServer.hasFile(fileName)) return;
 
-        List<String> paths = masterServer.getPaths(fileName);
-        String peerPath = paths.get(0);
+            List<String> paths = masterServer.getPaths(fileName);
+            String peerPath = paths.get(0);
 
-        // connect with server
-        FDS peerServer =
-                (FDS)Naming.lookup("rmi://"+peerData+"/master");
-        String fileData = peerServer.read(fileName);
+            // connect with server
+            FDS peerServer =
+                    (FDS)Naming.lookup("rmi://"+peerPath+"/master");
+            String fileData = peerServer.read(fileName);
+            String newData = fileData + updatedData;
+            peerServer.update(fileName, newData);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
 
     }
 
     public static void main(String args[]) {
         String response;
-        Integer masterPORT = 1901;
-        String masterIP = "localhost";
-        Integer port = 1900;
-        String IP = "localhost";
 
         try
         {
+            PeerClient clientServer = new PeerClient();
+
             Master masterAccess =
-                    (Master)Naming.lookup("rmi://"+masterIP+":"+masterPORT+"/master");
+                    (Master)Naming.lookup("rmi://"+clientServer.masterIP+":"+clientServer.masterPORT+"/master");
             System.out.println(masterAccess);
             // create
-            createFile(masterAccess);
-
-
-
+            clientServer.createFile(masterAccess);
         }
         catch(Exception ae)
         {
